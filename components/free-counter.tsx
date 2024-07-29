d9ff832f549ff434f1d528344887483cc2222b8c
Furkan Yorulmaz <furkan472143@gmail.com>
2024-07-29 09:46:39 +0300

    upgrade button finished

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -1,3 +1,11 @@
+"use client";
+import{ useEffect, useState } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { MAX_FILE_COUNT } from "@/constants";
+import { Progress } from "@/components/ui/progress";
+import { Button } from "@/components/ui/button";
+import { Zap } from "lucide-react";
+
 interface FreeCounterProps {
     apiLimitCount: number;
 }
@@ -5,9 +13,32 @@ interface FreeCounterProps {
 export const FreeCounter = ({
     apiLimitCount=0
 }:FreeCounterProps)=> {
+    const [mounted,setMounted] = useState(false);
+    useEffect(()=>{
+        setMounted(true);
+    },[]);
+    if(!mounted){
+        return null;
+    }
     return(
-        <div>
-        Free Counter
+        <div className="px-3">
+       <Card className="bg-white/10 broder-0">
+         <CardContent className="py-6">
+           <div className="text-center text-sm text-white mb-4 space-y-2">
+              <p>
+                {apiLimitCount}/{MAX_FILE_COUNT} Free Generations
+              </p>
+              <Progress
+              className="h-3"
+              value={apiLimitCount/MAX_FILE_COUNT * 100}
+              />
+           </div>
+           <Button className="w-full" variant="premium">
+                Upgrade Now
+                <Zap className="w-4 h-4 ml-2 fill-white"/>
+           </Button>
+         </CardContent>
+       </Card>
         </div>
     )
 }
