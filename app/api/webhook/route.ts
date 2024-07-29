633eef97214fe1fd61b086648c71273fcd109fa7
Furkan Yorulmaz <furkan472143@gmail.com>
2024-07-29 16:55:59 +0300

    .

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -20,4 +20,22 @@ process.env.STRIPE_WEBHOOK_SECRET!
 } catch (error: any) {
     return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
 }
+const session = event.data.object as Stripe.Checkout.Session;
+if (event.type === "checkout.session.completed") {
+    const subscription = await stripe.subscriptions.retrieve(session.subscription as string);
+    if(!session?.metadata?.userId){
+        return new NextResponse("User id is required", {status: 400});
+    }
+    await prismadb.userSubscription.create({
+        data: {
+            userId: session?.metadata?.userId,
+            stripeCustomerId: subscription.customer as string,
+            stripeSubscriptionId: subscription.id,
+            stripePriceId: subscription.items.data[0].price.id,
+            strripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
+        },
+    });
+ }
+
+ 
 }
